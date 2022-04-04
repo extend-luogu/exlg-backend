9564815dde0c65f0e968a02a8abfa83c79d6df86
汪心禾 <wangxinhe06@gmail.com>
2022-04-04 14:10:11 +0800

    Prettify index.js
    Thank @undefined-moe

--- index.js
+++ index.js
@@ -8,19 +8,28 @@ const key = 'badge';
 const app = express();
 const client = createClient();
 
-const dict = (keys, values) => Object.assign(...keys.map((k, i) => ({ [k]: values[i] })));
-
-client.on('error', (err) => console.error('Redis Client Error', err)); // eslint-disable-line no-console
+client.on('error', (err) => {
+  console.error('Redis Client Error', err); // eslint-disable-line no-console
+});
 client.connect();
 
 app.use(express.json());
 
 app.get('/get', async (req, res) => {
-  res.send(dict(req.body, await client.mGet(req.body.map((k) => `${namespace}:${k}:${key}`))));
+  const q = await client.mGet(
+    req.body.map((k) => `${namespace}:${k}:${key}`),
+  );
+  res.send(
+    Object.fromEntries(
+      req.body.map((k, i) => [k, q[i]]),
+    ),
+  );
 });
 
 app.get('/set', async (req, res) => {
-  await client.mSet(dict(Object.keys(req.body).map((k) => `${namespace}:${k}:${key}`), Object.values(req.body)));
+  await client.mSet(
+    Object.keys(req.body).map((k) => [`${namespace}:${k}:${key}`, req.body[k]]),
+  );
   res.send(req.body);
 });
 
