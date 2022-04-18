f66efb3c2f7f0cf7f1dda7a3dea732abae1bcd4b
汪心禾 <wangxinhe06@gmail.com>
2022-04-19 01:00:01 +0800

    Update import_activation.py

--- import_activation.py
+++ import_activation.py
@@ -3,10 +3,8 @@ from sys import stdin
 
 from redis import Redis
 
-from badge import ACTIVATION_KEY, key_to
-
 redis = Redis()
 
 for line in stdin.readlines():
     if line.strip():
-        redis.rpush(key_to(ACTIVATION_KEY), line.strip())
+        redis.rpush("exlg:activation", line.strip())
