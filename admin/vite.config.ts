0f338d94c42f932b9789b97c7673cee97b030025
汪心禾 <wangxinhe06@gmail.com>
2022-06-13 23:29:05 +0800

    ci(coverage): upload Vitest report to Codecov

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from "url";
 
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
@@ -9,6 +9,11 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  test: {
+    coverage: {
+      reporter: ["text", "json", "html"],
     },
   },
 });
